b79253e6123d6fbfdbc8e108172a6bf2fb73a520
Markings <27899906+CorruptedCodes@users.noreply.github.com>
2021-08-15 23:03:43 -0400

    Update PlayerJoinListener.java

--- src/main/java/me/markings/bubble/listeners/PlayerJoinListener.java
+++ src/main/java/me/markings/bubble/listeners/PlayerJoinListener.java
@@ -1,7 +1,6 @@
 package me.markings.bubble.listeners;
 
 import me.markings.bubble.settings.Settings;
-import me.markings.bubble.util.MessageUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -16,19 +15,17 @@ public class PlayerJoinListener implements Listener {
 	@EventHandler
 	public void onJoin(final PlayerJoinEvent event) {
 		final Player player = event.getPlayer();
+
 		final List<String> messages = Settings.WelcomeSettings.JOIN_MOTD;
 		final List<String> broadcastMessages = Settings.WelcomeSettings.JOIN_BROADCAST;
+
 		if (Settings.WelcomeSettings.ENABLE_JOIN_MOTD)
-			for (final String message : messages) {
-				MessageUtils.checkForPlaceholders(message);
-				Common.tellLater(Settings.WelcomeSettings.MOTD_DELAY.getTimeTicks(), player, Variables.replace(message, player));
-			}
+			messages.forEach(message -> Common.tellLater(Settings.WelcomeSettings
+					.MOTD_DELAY.getTimeTicks(), player, Variables.replace(message, player)));
 
 		if (Settings.WelcomeSettings.ENABLE_JOIN_BROADCASTS && !player.hasPlayedBefore())
-			for (int i = 0; i < broadcastMessages.toArray().length; i++) {
-				MessageUtils.checkForPlaceholders(broadcastMessages.get(i));
+			for (int i = 0; i < broadcastMessages.toArray().length; i++)
 				Common.broadcast(Variables.replace(broadcastMessages.get(i), player));
-			}
 	}
 
 }
