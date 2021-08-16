4b00b276aa8e6cba35900feb80685e39f8da4f32
Markings <27899906+CorruptedCodes@users.noreply.github.com>
2021-08-15 23:04:35 -0400

    Updated to support previous changes

--- src/main/java/me/markings/bubble/util/MessageUtils.java
+++ src/main/java/me/markings/bubble/util/MessageUtils.java
@@ -1,33 +1,26 @@
 package me.markings.bubble.util;
 
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
 import org.mineacademy.fo.Common;
 
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class MessageUtils {
 
-	private static final String titlePrefix = "[title]";
-	private static final String bossbarPrefix = "[bossbar]";
-	private static final String actionbarPrefix = "[actionbar]";
-	private static final String toastPrefix = "[toast]";
-	private static final String commandPrefix = "[command]";
+	public static String format(final String message) {
+		final String chatLinePlaceholder = "<chat_line>";
+		final String smoothLinePlaceholder = "<smooth_line>";
+		if (message.contains(chatLinePlaceholder))
+			return message.replace(chatLinePlaceholder, Common.chatLine());
+		else if (message.contains(smoothLinePlaceholder))
+			return message.replace(smoothLinePlaceholder, Common.chatLineSmooth());
 
-	public static void checkForPlaceholders(final String message) {
-		if (message.startsWith(titlePrefix))
-			Common.dispatchCommand(null, "bu notify all title "
-					+ message.replace(titlePrefix, ""));
+		return message;
+	}
 
-		else if (message.startsWith(bossbarPrefix))
-			Common.dispatchCommand(null, "bu notify all bossbar "
-					+ message.replace(bossbarPrefix, ""));
-
-		else if (message.startsWith(actionbarPrefix))
-			Common.dispatchCommand(null, "bu notify all actionbar "
-					+ message.replace(actionbarPrefix, ""));
-
-		else if (message.startsWith(toastPrefix))
-			Common.dispatchCommand(null, "bu notify all toast "
-					+ message.replace(toastPrefix, ""));
-
-		else if (message.startsWith(commandPrefix))
-			Common.dispatchCommand(null, message.replace(commandPrefix, ""));
+	public static void executePlaceholders(final String message) {
+		final String commandPlaceholder = "<command>";
+		if (message.startsWith(commandPlaceholder))
+			Common.dispatchCommand(null, message.replace(commandPlaceholder, ""));
 	}
 }
