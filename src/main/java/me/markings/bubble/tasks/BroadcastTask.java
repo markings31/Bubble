62cffdd1ef0577fa0ece51594c6c8d211cb1be25
Markings <27899906+CorruptedCodes@users.noreply.github.com>
2021-08-15 23:01:28 -0400

    Added world selection capability and improved optimization.
    Note: World selection capability may be incomplete.

--- src/main/java/me/markings/bubble/tasks/BroadcastTask.java
+++ src/main/java/me/markings/bubble/tasks/BroadcastTask.java
@@ -2,7 +2,6 @@ package me.markings.bubble.tasks;
 
 import me.markings.bubble.settings.Settings;
 import me.markings.bubble.util.MessageUtils;
-import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
 import org.mineacademy.fo.ChatUtil;
 import org.mineacademy.fo.Common;
@@ -15,42 +14,36 @@ import java.util.List;
 public class BroadcastTask extends BukkitRunnable {
 
 	private static int index;
-	private static final String titlePrefix = "[title]";
-	private static final String bossbarPrefix = "[bossbar]";
-	private static final String actionbarPrefix = "[actionbar]";
-	private static final String toastPrefix = "[toast]";
-	private static final String commandPrefix = "[command]";
-	private static final String centerPlaceholder = "<center>";
 
 	@Override
 	public void run() {
-		final List<String> messages;
-		final String broadcastSound = Settings.BroadcastSettings.BROADCAST_SOUND;
 		if (Settings.BroadcastSettings.ENABLE_BROADCASTS && !Remain.getOnlinePlayers().isEmpty()) {
+			final List<String> worlds = Settings.BroadcastSettings.BROADCAST_WORLDS;
+
 			final List<List<String>> orderedMsgList = Settings.BroadcastSettings.MESSAGE_LIST;
-			messages = Settings.BroadcastSettings.RANDOM_MESSAGE ? RandomUtil.nextItem(orderedMsgList) : orderedMsgList.get(index);
+			final List<String> messages = Settings.BroadcastSettings.RANDOM_MESSAGE ?
+					RandomUtil.nextItem(orderedMsgList) : orderedMsgList.get(index);
 
-			// TODO: Add support for '<smooth_line>' placeholder.
-			for (String message : messages)
-				if (message.contains(titlePrefix) || message.contains(bossbarPrefix) || message.contains(actionbarPrefix)
-						|| message.contains(toastPrefix) || message.contains(commandPrefix)) {
+			messages.forEach(message -> {
+				MessageUtils.executePlaceholders(message);
+				worlds.forEach(world -> Remain.getOnlinePlayers().forEach(player -> {
+					if (player.getWorld().getName().equals(world)) {
+						if (!ChatUtil.isInteractive(message))
+							Common.tellNoPrefix(player,
+									MessageUtils.format(Settings.BroadcastSettings.HEADER), "&f",
+									message, "&f", MessageUtils.format(Settings.BroadcastSettings.FOOTER));
+						else
+							Common.tellNoPrefix(player, message);
 
-					MessageUtils.checkForPlaceholders(message);
-
-					if (messages.indexOf(message) == messages.size())
-						break;
-					
-				} else {
-					message = message.startsWith(centerPlaceholder) ? ChatUtil.center(message).replace(centerPlaceholder, "") : message;
-					for (final Player player : Remain.getOnlinePlayers()) {
-						Common.tellNoPrefix(player, "&f", message, "&f");
-
-						CompSound.valueOf(broadcastSound).play(player);
+						CompSound.valueOf(Settings.BroadcastSettings.BROADCAST_SOUND).play(player);
 					}
-				}
+				}));
+			});
+
 			updateIndex(orderedMsgList);
 		}
 	}
+
 
 	private static void updateIndex(final List<List<String>> messages) {
 		index++;
